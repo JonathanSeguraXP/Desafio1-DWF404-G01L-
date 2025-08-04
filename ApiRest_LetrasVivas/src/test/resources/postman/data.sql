4d2abf8aba04b5aa8b06e8116b1e81d50b9824bd
JonathanSeguraXP <jonathansegura.cv@gmail.com>
2025-08-03 23:16:39 -0600

    Modificando parte 3

--- ApiRest_LetrasVivas/src/test/resources/postman/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO books (title, author, publication_year) VALUES
-('El Alquimista', 'Paulo Coelho', 1988),
-('1984', 'George Orwell', 1949),
-('Cien años de soledad', 'Gabriel García Márquez', 1967);
