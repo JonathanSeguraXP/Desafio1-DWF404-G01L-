14725dbfe1827eb52d655e60c89eb24e9412c9bc
JonathanSeguraXP <jonathansegura.cv@gmail.com>
2025-08-03 23:58:56 -0600

    update final

--- ApiRest_LetrasVivas/src/main/java/sv/edu/udb/config/SwaggerConf.java
+++ ApiRest_LetrasVivas/src/main/java/sv/edu/udb/config/SwaggerConf.java
@@ -1,22 +1,18 @@
-package sv.edu.udb.config;
+    package sv.edu.udb.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+    import io.swagger.v3.oas.models.OpenAPI;
+    import io.swagger.v3.oas.models.info.Info;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class SwaggerConf {
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Letras Vivas API")
-                        .version("1.0.0")
-                        .description("API para gestión de libros")
-                        .contact(new Contact()
-                                .name("Equipo Letras Vivas")
-                                .email("contacto@letrasvivas.com")));
+    @Configuration
+    public class SwaggerConf {
+        @Bean
+        public OpenAPI customOpenAPI() {
+            return new OpenAPI()
+                    .info(new Info()
+                            .title("Letras Vivas API")
+                            .version("1.0.0")
+                            .description("API para gestión de libros"));
+        }
     }
-}
